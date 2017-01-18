9040c371616d988f1ac31265549d188e1623dd80
Liudmila <liudmilamashkina@gmail.com>
2017-01-18 17:01:39 +0100

    fixed grass, inherit it from simple game object

--- src/GameObjects/Grass.h
+++ src/GameObjects/Grass.h
@@ -5,14 +5,14 @@
 #pragma warning(pop)
 
 #include "Utils/MacroCreate.h"
-#include "IGameObject.h"
+#include "SimpleGameObject.h"
 
 namespace cocos2d { class Node; }
 class b2Body;
 class GameWorld;
 class ParticlesObject;
 
-class Grass : public IGameObject
+class Grass : public SimpleGameObject
 {
 public:
     CREATE_FUNC_4(Grass, b2Body*, body, GameWorld*, world, cocos2d::Node*, node, const std::weak_ptr<ParticlesObject>&, particles);
@@ -21,9 +21,7 @@ public:
 
 	virtual void update(float delta) override;
 	
-	virtual b2Body* getBody() override { return _body; }
-    virtual b2Vec2 getPosition() override;
-    virtual GameObjectType getType() const override { return GameObjectType::Grass; }
+	virtual GameObjectType getType() const override { return GameObjectType::Grass; }
     virtual void onContactBegin(std::shared_ptr<IGameObject> obj) override;
     
 private:
@@ -32,9 +30,6 @@ private:
           cocos2d::Node* node,
           const std::weak_ptr<ParticlesObject>& particles);
     
-    b2Body* _body = nullptr;
-    GameWorld* _world = nullptr;
-    cocos2d::Node* _node = nullptr;
     std::weak_ptr<ParticlesObject> _particles;
 };
 
