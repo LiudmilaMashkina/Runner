17939b833cd603b305fcd7f16a784ac0de68f6f1
Liudmila <liudmilamashkina@gmail.com>
2017-01-29 13:33:31 +0100

    refactored button

---
+++ src/GUI2/Button.cpp
@@ -0,0 +1,73 @@
+#include "Button.h"
+#include "2d/CCSprite.h"
+#include "gui/BorderdNode.h"
+#include "base/CCEventListenerTouch.h"
+#include "base/CCEventDispatcher.h"
+
+namespace gui2
+{
+    USING_NS_CC;
+    
+	bool Button::initWith(const std::string &normal, const std::string &pressed)
+	{
+        if (!Node::init())
+            return false;
+        
+		Sprite* normalSprite = Sprite::create(normal);
+		normalSprite->setAnchorPoint(Vec2(0, 0));
+		normalSprite->setVisible(true);
+		_normal = normalSprite;
+
+		Sprite* pressedSprite = Sprite::create(pressed);
+		pressedSprite->setAnchorPoint(Vec2(0, 0));
+		pressedSprite->setVisible(false);
+		_pressed = pressedSprite;
+
+		setContentSize(normalSprite->getContentSize());
+		addChild(normalSprite);
+		addChild(pressedSprite);
+
+		auto touchListener = cocos2d::EventListenerTouchOneByOne::create();
+		touchListener->onTouchBegan = CC_CALLBACK_2(Button::onTouchBegan, this);
+		touchListener->onTouchMoved = CC_CALLBACK_2(Button::onTouchMoved, this);
+		touchListener->onTouchEnded = CC_CALLBACK_2(Button::onTouchEnded, this);
+		getEventDispatcher()->addEventListenerWithSceneGraphPriority(touchListener, this);
+        
+        return true;
+	}
+
+	bool Button::onTouchBegan(cocos2d::Touch * touch, cocos2d::Event * event)
+	{
+		Vec2 touchPos =	convertTouchToNodeSpace(touch);
+
+		cocos2d::Vec2 size = getContentSize();
+		if (touchPos.x < 0 || touchPos.x > size.x || touchPos.y < 0 || touchPos.y > size.y)
+			return false;
+
+		if (isVisible() == false)
+			return false;
+
+		_normal->setVisible(false);
+		_pressed->setVisible(true);
+
+		event->stopPropagation();
+		return true;
+	}
+
+	void Button::onTouchMoved(cocos2d::Touch * touch, cocos2d::Event * event)
+	{
+	}
+
+	void Button::onTouchEnded(cocos2d::Touch * touch, cocos2d::Event * event)
+	{
+		_normal->setVisible(true);
+		_pressed->setVisible(false);
+		_callback(this);
+	}
+
+	void Button::setCallback(const std::function<void(Button*)> callback)
+	{
+		_callback = callback;
+	}
+
+}
---
+++ src/GUI2/Button.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "2d/CCSprite.h"
+#include "utils/MacroCreate.h"
+
+namespace gui2
+{
+    class Button : public cocos2d::Node
+	{
+	public:
+        CC_CREATE_FUNC_2(Button, initWith,
+                         const std::string&, normal,
+                         const std::string&, pressed);
+        
+		void setCallback(const std::function<void(Button*)> callback);
+
+	private:
+		bool initWith(const std::string &normal, const std::string &pressed);
+
+		bool onTouchBegan(cocos2d::Touch *touch, cocos2d::Event *event);
+		void onTouchMoved(cocos2d::Touch *touch, cocos2d::Event *event);
+		void onTouchEnded(cocos2d::Touch *touch, cocos2d::Event *event);
+
+		cocos2d::Sprite* _normal = nullptr;
+		cocos2d::Sprite* _pressed = nullptr;
+
+		std::function<void(Button*)> _callback;
+	};
+
+}
+
