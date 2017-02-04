c26a518eaec8f9127be44e5147c61157e5ca761c
Liudmila <liudmilamashkina@gmail.com>
2017-02-04 14:13:31 +0100

    foo

---
+++ projects/ios/Runner/Runner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Runner
+//
+//  Created by Luda on 04/02/2017.
+//  Copyright © 2017 Luda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppController.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppController class]));
+    }
+}
